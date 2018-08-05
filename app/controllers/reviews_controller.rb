05c33dd64d466388bbefce4035e7bfa34509f245
shadid12 <shadidhaque2012@gmail.com>
2018-08-04 23:14:55 -0400

    last commit before rebooting mac

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+
+    before_action :find_play
+
+    def new
+        @review = Review.new
+    end
+    
+    def create
+        @review = Review.new()
+        @review.play_id = @play.id
+        @review.user_id = current_user.id
+
+        if @review.save
+            redirect_to root_path(@play)
+        else 
+            render 'new'
+        end
+
+    end
+
+    private
+        def review_params
+            params.require(:review).permit(:rating, :comment)
+        end
+
+        def find_play
+            @play = Play.find(params[:play_id])
+
+        end
+        
+        
+
+end
